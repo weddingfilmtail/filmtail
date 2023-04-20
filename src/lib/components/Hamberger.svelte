8d32b5fbff7ca94a46731623130c679e6804cb90
heygwangjin <heygwangjin@gmail.com>
2023-04-20 23:34:01 +0900

    feat: Add hamburger button component and mobile menu functionality to Header component

---
+++ src/lib/components/Hamberger.svelte
@@ -0,0 +1,21 @@
+<!-- Hamburger.svelte -->
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('toggle');
+	}
+</script>
+
+<button
+	on:click={handleClick}
+	class="focus:shadow-outline block focus:outline-none lg:hidden"
+	type="button"
+>
+	<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M4 5h16c1.1 0 2-.9 2-2s-.9-2-2-2H4C2.9 1 2 1.9 2 3s.9 2 2 2zm0 6h16c1.1 0 2-.9 2-2s-.9-2-2-2H4c-1.1 0-2 .9-2 2s.9 2 2 2zm0 6h16c1.1 0 2-.9 2-2s-.9-2-2-2H4c-1.1 0-2 .9-2 2s.9 2 2 2z"
+		/>
+	</svg>
+</button>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,38 @@
-<!-- Navbar.svelte -->
 <script>
 	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	import Hamberger from './Hamberger.svelte';
 	import Logo from './Logo.svelte';
 	import Navbar from './Navbar.svelte';
+
+	let isMobileMenuOpen = false;
+
+	function handleToggle() {
+		isMobileMenuOpen = !isMobileMenuOpen;
+	}
 </script>
 
 <header
-	class="flex w-full items-center justify-between px-4 py-10 xl:px-36 {$page.url.pathname === '/'
+	class="flex w-full items-center justify-between p-4 xl:px-36 xl:py-10 {$page.url.pathname === '/'
 		? 'fixed left-0 top-0 z-10 bg-opacity-0 text-white'
 		: ''}"
 >
 	<Logo />
 	<Navbar />
+	<Hamberger on:toggle={handleToggle} />
 </header>
+
+{#if isMobileMenuOpen}
+	<div
+		class="fixed left-0 top-0 z-20 h-full w-full bg-orange-200"
+		class:translate-x-0={isMobileMenuOpen}
+		transition:fade
+	>
+		<div class="flex flex-col justify-center pt-4">
+			<button class="font-semibold" on:click={handleToggle}>Close</button>
+		</div>
+	</div>
+{/if}
 
 <style lang="postcss">
 	/* nav ul li a.active {
