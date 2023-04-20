b508077b25f0e6bc7ca11df7f508b7ce4449762b
heygwangjin <heygwangjin@gmail.com>
2023-04-20 21:38:22 +0900

    feat: Add Navbar

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,3 @@
+<a href="/">
+	<img src="/logo.png" alt="logo" class="w-40" />
+</a>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,53 +1,31 @@
 <!-- Navbar.svelte -->
 <script>
-	let currentPage = 'home'; // Default selected page
+	import { page } from '$app/stores';
+	import Logo from './Logo.svelte';
 </script>
 
-<div class="navbar">
-	<a class="nav-item" on:click={() => (currentPage = 'home')} class:active={currentPage === 'home'}>
-		Home
-	</a>
-	<a
-		class="nav-item"
-		on:click={() => (currentPage = 'portfolio')}
-		class:active={currentPage === 'portfolio'}
-	>
-		Portfolio
-	</a>
-	<a
-		class="nav-item"
-		on:click={() => (currentPage = 'product')}
-		class:active={currentPage === 'product'}
-	>
-		Product
-	</a>
-	<a
-		class="nav-item"
-		on:click={() => (currentPage = 'contact')}
-		class:active={currentPage === 'contact'}
-	>
-		Contact
-	</a>
-</div>
+<header class="flex items-center justify-between px-36 py-10">
+	<Logo />
+	<nav>
+		<ul class="flex gap-16 text-lg font-semibold">
+			<li>
+				<a href="/" class:active={$page.url.pathname === '/'}> HOME </a>
+			</li>
+			<li>
+				<a href="/portfolio" class:active={$page.url.pathname === 'portfolio'}> PORTFOLIO </a>
+			</li>
+			<li>
+				<a href="/product" class:active={$page.url.pathname === 'product'}> PRODUCT </a>
+			</li>
+			<li>
+				<a href="/contact" class:active={$page.url.pathname === 'contact'}> CONTACT </a>
+			</li>
+		</ul>
+	</nav>
+</header>
 
-<style>
-	.navbar {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-		background-color: #333;
-		padding: 10px 0;
-	}
-
-	.nav-item {
-		color: #fff;
-		text-decoration: none;
-		font-size: 1.2rem;
-		cursor: pointer;
-	}
-
-	.nav-item:hover,
-	.nav-item.active {
-		color: #50d890;
-	}
+<style lang="postcss">
+	/* nav ul li a.active {
+		color: #ff3e00;
+	} */
 </style>
