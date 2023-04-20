66db1f91c5d173dad5d68316f9da13be10221a78
heygwangjin <heygwangjin@gmail.com>
2023-04-20 22:21:24 +0900

    feat: Add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,45 @@
+<!-- Footer.svelte -->
+<script>
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
+</script>
+
+<footer class="flex w-full flex-col items-center justify-between py-12 sm:flex-row sm:px-36">
+	<p class="">Copyrights &copy; Filmtail 2023 | All rights Reserved</p>
+	<div class="flex gap-4">
+		<button class="font-semibold" on:click={scrollToTop}> Back to Top </button>
+		<button class="font-semibold" on:click={scrollToTop}>
+			<svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<g id="Complete">
+					<g id="arrow-up">
+						<g>
+							<polyline
+								data-name="Right"
+								fill="none"
+								id="Right-2"
+								points="7 7.5 12 2.5 17 7.5"
+								stroke="#000000"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+							/>
+
+							<line
+								fill="none"
+								stroke="#000000"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								x1="12"
+								x2="12"
+								y1="21.3"
+								y2="4.8"
+							/>
+						</g>
+					</g>
+				</g>
+			</svg>
+		</button>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 </script>
 
 <Navbar />
 <slot />
+<Footer />
