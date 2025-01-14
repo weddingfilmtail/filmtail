f86c75e333d5c3634f902a4b33edc34e24fdce1a
heygwangjin <heygwangjin@gmail.com>
2025-01-14 22:52:07 +0900

    chore: Rename variable

--- src/routes/portfolio/+page.js
+++ src/routes/portfolio/+page.js
@@ -3,13 +3,13 @@ import { supabase } from '$lib/supabase.js';
 /** @type {import('./$types').PageLoad} */
 export const load = async () => {
 	const fetchPortfolios = async () => {
-		const { data: videos, error } = await supabase.from('portfolio').select('*');
+		const { data: portfolios, error } = await supabase.from('portfolio').select('*');
 
 		if (error) {
 			throw error;
 		}
 
-		return videos;
+		return portfolios;
 	};
 
 	return {
