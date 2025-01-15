f7d93d7585c9c76e3bd21b488fdc6281d037565e
heygwangjin <heygwangjin@gmail.com>
2025-01-15 19:07:38 +0900

    style: Apply futura fonts

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <nav class="hidden lg:block">
-	<ul class="mr-4 flex items-center gap-16 text-xs font-bold tracking-widest">
+	<ul class="mr-4 flex items-center gap-16 text-sm tracking-widest">
 		<li>
 			<a href="/" class:active={$page.url.pathname === '/'}> HOME </a>
 		</li>
@@ -12,19 +12,19 @@
 			<a href="/about" class:active={$page.url.pathname === 'about'}> ABOUT US </a>
 		</li> -->
 		<li>
-			<a href="/portfolio" class:active={$page.url.pathname === 'portfolio'}> PORTFOLIO </a>
+			<a href="/portfolio" class:active={$page.url.pathname === '/portfolio'}> PORTFOLIO </a>
 		</li>
 		<li>
-			<a href="/product" class:active={$page.url.pathname === 'product'}> PRODUCT </a>
+			<a href="/product" class:active={$page.url.pathname === '/product'}> PRODUCT </a>
 		</li>
 		<li>
-			<a href="/contact" class:active={$page.url.pathname === 'contact'}> CONTACT </a>
+			<a href="/contact" class:active={$page.url.pathname === '/contact'}> CONTACT </a>
 		</li>
 	</ul>
 </nav>
 
 <style lang="postcss">
-	/* nav ul li a.active {
-		color: #ff3e00;
-	} */
+	nav ul li a.active {
+		font-family: 'futura condensed medium', serif;
+	}
 </style>
