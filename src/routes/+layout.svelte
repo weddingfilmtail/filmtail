89340b20afb65c3bf413688de25b57122893aef4
heygwangjin <heygwangjin@gmail.com>
2025-01-17 19:51:10 +0900

    feat: Prevent double touch zoom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,23 @@
 	import Header from '$lib/components/Header.svelte';
 	import { page } from '$app/stores';
 	import '../app.css';
+
+	let lastTouchEnd = 0;
+
+	const handleTouchEnd = (event) => {
+		const now = new Date().getTime();
+		if (now - lastTouchEnd <= 300) {
+			event.preventDefault();
+		}
+		lastTouchEnd = now;
+	};
 </script>
 
 <Header />
-<main class="min-h-screen w-full {$page.url.pathname !== '/' ? 'mx-auto max-w-6xl' : ''}">
+<main
+	class="min-h-screen w-full {$page.url.pathname !== '/' ? 'mx-auto max-w-6xl' : ''}"
+	on:touchend={handleTouchEnd}
+>
 	<slot />
 </main>
 <Footer />
