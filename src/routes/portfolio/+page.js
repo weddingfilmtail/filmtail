0c3a9a9dbdbd8a20adfeeedbb5c8f912666ef5ee
heygwangjin <heygwangjin@gmail.com>
2025-01-17 14:00:29 +0900

    feat: Add portfolio page

--- src/routes/portfolio/+page.js
+++ src/routes/portfolio/+page.js
@@ -9,7 +9,12 @@ export const load = async () => {
 			throw error;
 		}
 
-		return portfolios;
+		portfolios.forEach(async (portfolio) => {
+			const { data } = supabase.storage.from('images').getPublicUrl(portfolio.image_key);
+			portfolio.image_key = data.publicUrl;
+		});
+
+		return portfolios.sort((a, b) => a.priority - b.priority);
 	};
 
 	return {
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -15,24 +15,23 @@
 	}
 </script>
 
-<div class="p-8 md:px-32 md:py-16 xl:mx-14 2xl:mx-36">
-	<div class="grid grid-cols-1 gap-x-32 gap-y-12 md:gap-y-24 xl:grid-cols-2">
+<div class="pt-52 xl:pt-32">
+	<div class="grid grid-cols-1 gap-y-52 xl:gap-y-24">
 		{#each portfolios as portfolio, i}
 			<button
-				class="flex items-center justify-center rounded border shadow-2xl"
 				on:click={() => {
 					selectedPortfolio = portfolio;
 					showPortfolio = true;
 				}}
 				in:fade={{ duration: 300, delay: 200 * i }}
 			>
-				<img src={portfolio.image_key} alt="portfolio" class="w-full rounded" />
+				<img src={portfolio.image_key} alt={portfolio.title} class="h-full w-full object-cover" />
 			</button>
 		{/each}
 	</div>
 	{#if showPortfolio}
 		<div
-			class="fixed left-0 top-0 z-20 flex h-full w-full items-center justify-center bg-black bg-opacity-90"
+			class="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-90"
 			on:click={() => (showPortfolio = false)}
 			class:translate-x-0={showPortfolio}
 			transition:fade
