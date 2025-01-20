920e81b48ab4b532b2ba0bbdfd7476476c081310
heygwangjin <heygwangjin@gmail.com>
2025-01-20 17:38:30 +0900

    feat: Add autoplay for embedded youtube

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -5,7 +5,7 @@
 <iframe
 	width="80%"
 	height="70%"
-	src={portfolio.url}
+	src={`${portfolio.url}${portfolio.url.includes('?') ? '&' : '?'}autoplay=1&mute=1`}
 	title={portfolio.title}
 	frameborder="0"
 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
