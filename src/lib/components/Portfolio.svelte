c8ce17d3170e8054976fbb21b29d7bd6af474df0
heygwangjin <heygwangjin@gmail.com>
2025-01-22 13:56:54 +0900

    refactor: Convert youtube url to embedded url

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -1,11 +1,24 @@
 <script>
 	export let portfolio = {};
+
+	function getYoutubeEmbedUrl(url) {
+		try {
+			const baseUrl = portfolio.url.slice(0, 23);
+			const videoId = portfolio.url.slice(24);
+			return `${baseUrl}/embed/${videoId}`.trim();
+		} catch (error) {
+			console.error('Youtube URL 변환 중 오류', error);
+			return url;
+		}
+	}
+
+	const embeddedUrl = getYoutubeEmbedUrl(portfolio.url);
 </script>
 
 <iframe
 	width="80%"
 	height="70%"
-	src={`${portfolio.url}${portfolio.url.includes('?') ? '&' : '?'}autoplay=1&mute=1`}
+	src={`${embeddedUrl}${portfolio.url.includes('?') ? '&' : '?'}autoplay=1&mute=1`}
 	title={portfolio.title}
 	frameborder="0"
 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
